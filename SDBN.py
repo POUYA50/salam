c98c37fdff76a90321b210a763da2832fcaa69bf
POUYA50 <cocholate865@gmail.com>
2024-08-18 08:24:29 -0700

    Add files via upload

---
+++ SDBN.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.color('yellow')
+turtle.forward(90)
+turtle.left(72)
+turtle.color('red')
+turtle.forward(90)
+turtle.left(72)
+turtle.color('blue')
+turtle.forward(90)
+turtle.left(72)
+turtle.color('pink')
+turtle.forward(90)
+turtle.left(72)
+turtle.color('grey')
+turtle.forward(90)
+rang=['red','blue','yellow','purple','green']
+
